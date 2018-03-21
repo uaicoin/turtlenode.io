75e840bf02cf9a643c8a4fd5ac73e6120a124259
Brandon Lehmann <blehmann@velocity.org>
2018-03-21 18:36:13 -0400

    Added nodes in Singapore

--- js/config.js
+++ js/config.js
@@ -8,5 +8,8 @@ const nodes = [
   { name: 'US-West-3', hostname: 'node-3.sfo.turtlenode.io', port: 11898, region: 'US West' },
   { name: 'EU-AMS-1', hostname: 'node-1.ams.turtlenode.io', port: 11898, region: 'Europe' },
   { name: 'EU-AMS-2', hostname: 'node-2.ams.turtlenode.io', port: 11898, region: 'Europe' },
-  { name: 'EU-AMS-3', hostname: 'node-3.ams.turtlenode.io', port: 11898, region: 'Europe' }
+  { name: 'EU-AMS-3', hostname: 'node-3.ams.turtlenode.io', port: 11898, region: 'Europe' },
+  { name: 'AS-SIN-1', hostname: 'node-1.sin.turtlenode.io', port: 11898, region: 'Asia' },
+  { name: 'AS-SIN-2', hostname: 'node-2.sin.turtlenode.io', port: 11898, region: 'Asia' },
+  { name: 'AS-SIN-3', hostname: 'node-3.sin.turtlenode.io', port: 11898, region: 'Asia' }
 ]
