1a4285f5de6f8a4c71e26c1b50153835dd779509
Brandon Lehmann <blehmann@velocity.org>
2018-03-27 10:06:26 -0400

    Added listing for seed.turtlenode.io that has been included in the turtlecoin master branch.

--- js/config.js
+++ js/config.js
@@ -11,5 +11,6 @@ const nodes = [
   { name: 'EU-AMS-3', hostname: 'node-3.ams.turtlenode.io', port: 11898, region: 'Europe' },
   { name: 'AS-SIN-1', hostname: 'node-1.sin.turtlenode.io', port: 11898, region: 'Asia' },
   { name: 'AS-SIN-2', hostname: 'node-2.sin.turtlenode.io', port: 11898, region: 'Asia' },
-  { name: 'AS-SIN-3', hostname: 'node-3.sin.turtlenode.io', port: 11898, region: 'Asia' }
+  { name: 'AS-SIN-3', hostname: 'node-3.sin.turtlenode.io', port: 11898, region: 'Asia' },
+  { name: 'SEED', hostname: 'seed.turtlenode.io', port: 11898, region: 'US East' }
 ]
