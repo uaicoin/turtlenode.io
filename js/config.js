941c0eb3c791a0f36ca596e863d7901280df3997
dmaillard <david@dmaillard.com>
2018-04-11 19:05:30 -0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 const apiInterface = 'https://uaicoin.github.io/uainode/'
 const nodes = [
-  { name: 'Localhost', hostname: '127.0.0.1', port: 21898, region: 'US East3' },
-  { name: 'SEED 18.232.33.114', hostname: '18.232.33.114', port: 21898, region: 'US East2' },
-  { name: 'SEED 18.234.17.54', hostname: '18.234.17.54', port: 21898, region: 'US East1' }
+  { name: 'localhost', hostname: '127.0.0.1', port: 21898, region: 'localhost' },
+  { name: '18.232.33.114', hostname: '18.232.33.114', port: 21898, region: 'us-east-1b' },
+  { name: '18.234.17.54', hostname: '18.234.17.54', port: 21898, region: 'us-east-1b' }
 ]
