892954fa0045c94de9167a185727a4837fb41c2e
dmaillard <david@dmaillard.com>
2018-04-11 18:32:34 -0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,16 +1,6 @@
-const apiInterface = 'https://api.turtlenode.io/'
+const apiInterface = 'https://uaicoin.github.io/uainode/'
 const nodes = [
-  { name: 'US-East-1', hostname: 'node-1.nyc.turtlenode.io', port: 11898, region: 'US East' },
-  { name: 'US-East-2', hostname: 'node-2.nyc.turtlenode.io', port: 11898, region: 'US East' },
-  { name: 'US-East-3', hostname: 'node-3.nyc.turtlenode.io', port: 11898, region: 'US East' },
-  { name: 'US-West-1', hostname: 'node-1.sfo.turtlenode.io', port: 11898, region: 'US West' },
-  { name: 'US-West-2', hostname: 'node-2.sfo.turtlenode.io', port: 11898, region: 'US West' },
-  { name: 'US-West-3', hostname: 'node-3.sfo.turtlenode.io', port: 11898, region: 'US West' },
-  { name: 'EU-AMS-1', hostname: 'node-1.ams.turtlenode.io', port: 11898, region: 'Europe' },
-  { name: 'EU-AMS-2', hostname: 'node-2.ams.turtlenode.io', port: 11898, region: 'Europe' },
-  { name: 'EU-AMS-3', hostname: 'node-3.ams.turtlenode.io', port: 11898, region: 'Europe' },
-  { name: 'AS-SIN-1', hostname: 'node-1.sin.turtlenode.io', port: 11898, region: 'Asia' },
-  { name: 'AS-SIN-2', hostname: 'node-2.sin.turtlenode.io', port: 11898, region: 'Asia' },
-  { name: 'AS-SIN-3', hostname: 'node-3.sin.turtlenode.io', port: 11898, region: 'Asia' },
-  { name: 'SEED', hostname: 'seed.turtlenode.io', port: 11898, region: 'US East' }
+  { name: 'US-East-1', hostname: '18.232.33.114', port: 21898, region: 'US East' },
+  { name: 'US-East-2', hostname: '127.0.0.1', port: 21898, region: 'US East' },
+  { name: 'SEED', hostname: '18.234.17.54', port: 21898, region: 'US East' }
 ]
